7cbb60a5f332a5c940f92f2992b00c0eb490d22f
Ricardo Rodriguez <peyotex@gmail.com>
2021-01-06 16:00:07 -0600

    Revert "First UI"
    This reverts commit f2bcafff41170d7f21dd4e96d00c61260cf22c25.

--- ui/lib/datagrid.js
+++ ui/lib/datagrid.js
@@ -1,29 +1,34 @@
 class c4tgrid {
     constructor(selector) {
         this.targetEl = $(selector);
-        this.targetEl.empty();
         this.id = this.targetEl.attr("id");
-        // this.rows = 50;
-        // this.colls = 26;
-        this.targetEl.append($('<input id="glbRows" name="glbRows" type="hidden" value="50">'));
-        this.targetEl.append($('<input id="glbColls" name="glbColls" type="hidden" value="26">'));
+        this.rows = 50;
+        this.colls = 26;
         this.targetEl.addClass('c4tgrid')
 
         this.table = $('<table class="c4tCore"><thead></thead><tbody></tbody></table>');
         this.create();
     }
-    add_key_event(main_class, inpt) {
 
+    i_to_coll(i) {
+        if (i < 26)
+            return String.fromCharCode(i + 65);
+        else
+            return String.fromCharCode(i / 26 + 64) + String.fromCharCode(i % 26 + 65);
+    }
+
+    int(i) {
+        return Number.parseInt(i, 10);
+    }
+
+    add_key_event(main_class, inpt) {
         inpt.keydown(function(e) {
-            var colls = int($('#glbColls').val());
-            var rows = int($('#glbRows').val());
-            var inp_r = int($(this).attr('r'));
-            var inp_c = int($(this).attr('c'));
+            var inp_r = main_class.int($(this).attr('r'));
+            var inp_c = main_class.int($(this).attr('c'));
             var n_l_c = inp_c - 1;
             var n_r_c = inp_c + 1;
             var n_u_r = inp_r - 1;
             var n_d_r = inp_r + 1;
-            globaldata[currentID]['data'][inp_r][inp_c] = $(this).text();
             switch (e.which) {
                 case 37:
                     /* arrow left */
@@ -33,7 +38,7 @@ class c4tgrid {
                     break;
                 case 9:
                     /* tab */
-                    if (n_r_c <= colls) {
+                    if (n_r_c <= main_class.colls) {
                         $('#' + main_class.id + "-row-" + inp_r + "-col-" + n_r_c).focus();
                     }
                     break;
@@ -45,14 +50,13 @@ class c4tgrid {
                     break;
                 case 39:
                     /* arrow right */
-                    if (n_r_c <= colls) {
+                    if (n_r_c < main_class.colls) {
                         $('#' + main_class.id + "-row-" + inp_r + "-col-" + n_r_c).focus();
                     } else {
-                        $('#glbColls').val(n_r_c);
+
                         main_class.add_coll(main_class, n_r_c);
-
+                        main_class.colls = n_r_c;
                         $('#' + main_class.id + "-row-" + inp_r + "-col-" + n_r_c).focus();
-
                     }
                     break;
                 case 46:
@@ -61,21 +65,20 @@ class c4tgrid {
                     break;
                 case 13:
                     /* return/enter */
-                    if (n_d_r <= rows) {
+                    if (n_d_r <= main_class.rows) {
                         $('#' + main_class.id + "-row-" + n_d_r + "-col-" + inp_c).focus();
                     } else {
-                        $('#glbRows').val(n_d_r);
                         main_class.add_row(main_class, n_d_r)
-
+                        main_class.rows = n_d_r;
                         $('#' + main_class.id + "-row-" + n_d_r + "-col-" + inp_c).focus();
                     }
                     break;
                 case 40:
                     /* arrow down */
-                    if (n_d_r <= rows) {
+                    if (n_d_r <= main_class.rows) {
                         $('#' + main_class.id + "-row-" + n_d_r + "-col-" + inp_c).focus();
                     } else {
-                        if (n_r_c <= colls)
+                        if (n_r_c <= main_class.colls)
                             $('#' + main_class.id + "-row-" + 1 + "-col-" + n_r_c).focus();
                     }
                     break;
@@ -92,109 +95,96 @@ class c4tgrid {
         var row_data = $("<tr/>");
         var r_num = $("<th/>").text(r);
         row_data.append(r_num);
-        var colls = int($('#glbColls').val());
-        var rows = int($('#glbRows').val());
-        globaldata[currentID]['data'][r] = [];
-        for (var c = 1; c <= colls; c++) {
-            // var td = $("<td/>");
-            globaldata[currentID]['data'][r][c] = '';
-            var inpt = $("<td/>", { id: main_class.id + "-row-" + r + "-col-" + c, r: r, c: c, class: 'inputx', contenteditable: 'true' });
+
+        for (var c = 0; c < main_class.colls; c++) {
+            var td = $("<td/>");
+            var inpt = $("<input/>", { id: main_class.id + "-row-" + r + "-col-" + c, r: r, c: c });
             main_class.add_key_event(main_class, inpt);
-            // td.append(inpt);
-            row_data.append(inpt);
+            td.append(inpt);
+            row_data.append(td);
         }
         tbody.append(row_data);
     }
 
     add_coll(main_class, c) {
         var thead = main_class.table.find('thead');
-        var colls = int($('#glbColls').val());
-        var rows = int($('#glbRows').val());
         var row_head = thead.find('tr');
-        globaldata[currentID]['colls'][c] = i_to_coll(c - 1);
         var th = $("<th/>", { id: main_class.id + "-col-" + c });
         var the_div = $("<div/>", { class: "relative" });
-        var the_spn = $("<span/>", { class: "colHeader" }).text(i_to_coll(c - 1));
-        // var the_sub_div = $("<div/>", { class: "manualColumnResizer", rel: c });
-        // the_sub_div.on('dblclick', function() {
-        //     var min = 50;
-        //     var size_new = 0;
-        //     var c = main_class.int($(this).attr('rel'));
-        //     for (var r = 1; r <= rows; r++) {
-        //         var size_tmp = $("#" + main_class.id + "-row-" + r + "-col-" + c).val().length;
-        //         if (size_tmp > size_new) {
-        //             size_new = size_tmp;
-        //         }
-        //     }
+        var the_spn = $("<span/>", { class: "colHeader" }).text(main_class.i_to_coll(c));
+        var the_sub_div = $("<div/>", { class: "manualColumnResizer", rel: c });
+        the_sub_div.dblclick(function() {
+            var min = 50;
+            var size_new = 0;
+            var c = main_class.int($(this).attr('rel'));
+            for (var r = 1; r <= main_class.rows; r++) {
+                var size_tmp = $("#" + main_class.id + "-row-" + r + "-col-" + c).val().length;
+                if (size_tmp > size_new) {
+                    size_new = size_tmp;
+                }
+            }
 
-        //     var ne_th = $("#" + main_class.id + "-col-" + c);
-        //     var fontSize = parseInt(ne_th.css('font-size'));
-        //     size_new = size_new * fontSize;
-        //     if (size_new > min) {
+            var ne_th = $("#" + main_class.id + "-col-" + c);
+            var fontSize = parseInt(ne_th.css('font-size'));
+            size_new = size_new * fontSize;
+            if (size_new > min) {
 
 
-        //         ne_th.width(size_new + 'px')
-        //     }
+                ne_th.width(size_new + 'px')
+            }
 
-        // });
+        });
         the_div.append(the_spn);
-        // the_div.append(the_sub_div);
+        the_div.append(the_sub_div);
         th.append(the_div);
         row_head.append(th);
         var tbody = main_class.table.find('tbody');
         tbody.find('tr').each(function(index, tr) {
             var r = parseInt(index) + 1;
-            globaldata[currentID]['data'][r][c] = '';
-            // var td = $("<td/>");
-            var inpt = $("<td/>", { id: main_class.id + "-row-" + r + "-col-" + c, r: r, c: c, class: 'inputx', contenteditable: 'true' });
+
+            var td = $("<td/>");
+            var inpt = $("<input/>", { id: main_class.id + "-row-" + r + "-col-" + c, r: r, c: c });
             main_class.add_key_event(main_class, inpt);
-            // td.append(inpt);
-            $(tr).append(inpt);
+            td.append(inpt);
+            $(tr).append(td);
         });
     }
 
+
     create() {
-        currentID = uuidv();
-        globaldata[currentID] = {};
-        globaldata[currentID]['colls'] = [];
-        globaldata[currentID]['data'] = [];
         this.targetEl.prepend(this.table);
         var thead = this.table.find('thead');
         var row_head = $("<tr/>");
         var main_class = this;
-        var colls = int($('#glbColls').val());
-        var rows = int($('#glbRows').val());
         row_head.append($("<th/>", { class: "rowHeader" }));
-        for (var c = 1; c <= colls; c++) {
-            globaldata[currentID]['colls'][c] = i_to_coll(c - 1);
+        for (var c = 0; c < this.colls; c++) {
             var th = $("<th/>", { id: this.id + "-col-" + c });
             var the_div = $("<div/>", { class: "relative" });
-            var the_spn = $("<span/>", { class: "colHeader" }).text(i_to_coll(c - 1));
-            // var the_sub_div = $("<div/>", { class: "manualColumnResizer", rel: c });
-            // the_sub_div.on('dblclick', function() {
-            //     var min = 50;
-            //     var size_new = 0;
-            //     var c = main_class.int($(this).attr('rel'));
-            //     var ne_th = $("#" + main_class.id + "-col-" + c);
-            //     var fontSize = 50;
-            //     for (var r = 1; r <= rows; r++) {
-            //         var size_tmp = $("#" + main_class.id + "-row-" + r + "-col-" + c).val().length;
-            //         if (size_tmp > size_new) {
-            //             size_new = size_tmp;
-            //             fontSize = parseInt($("#" + main_class.id + "-row-" + r + "-col-" + c).prop('scrollWidth'));
-            //         }
-            //     }
-            //     //var fontSize = parseInt(ne_th.css('font-size'));
-            //     size_new = fontSize;
-            //     if (size_new > min) {
+            var the_spn = $("<span/>", { class: "colHeader" }).text(this.i_to_coll(c));
+            var the_sub_div = $("<div/>", { class: "manualColumnResizer", rel: c });
+            the_sub_div.dblclick(function() {
+                var min = 50;
+                var size_new = 0;
+                var c = main_class.int($(this).attr('rel'));
+                for (var r = 1; r <= main_class.rows; r++) {
+                    var size_tmp = $("#" + main_class.id + "-row-" + r + "-col-" + c).val().length;
+                    if (size_tmp > size_new) {
+                        size_new = size_tmp;
+                    }
+                }
+
+                var ne_th = $("#" + main_class.id + "-col-" + c);
+                var fontSize = parseInt(ne_th.css('font-size'));
+                size_new = size_new * fontSize;
+                if (size_new > min) {
 
 
-            //         ne_th.width(size_new + 'px')
-            //     }
+                    ne_th.width(size_new + 'px')
+                }
 
-            // });
+            });
             the_div.append(the_spn);
-            // the_div.append(the_sub_div);
+            the_div.append(the_sub_div);
             th.append(the_div);
             row_head.append(th);
         }
@@ -202,7 +192,7 @@ class c4tgrid {
         thead.prepend(row_head);
 
         var tbody = this.table.find('tbody');
-        for (var r = 1; r <= rows; r++) {
+        for (var r = 1; r <= this.rows; r++) {
             this.add_row(main_class, r);
 
         }
