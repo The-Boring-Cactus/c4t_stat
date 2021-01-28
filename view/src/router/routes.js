ba6bb6abea308304866d01c9c9eea279d99ede1e
Ricardo Rodriguez <peyotex@gmail.com>
2021-01-28 16:51:02 -0600

    Data grid editor V0.5 done

--- view/src/router/routes.js
+++ view/src/router/routes.js
@@ -8,6 +8,17 @@ const routes = [{
                 import ('pages/Index.vue')
         }]
     },
+    {
+        path: '/peyote',
+        component: () =>
+            import ('layouts/MainLayout.vue'),
+        children: [{
+            path: '',
+            component: () =>
+                import ('components/DataGrid.vue'),
+            props: route => ({ dataId: route.query.id })
+        }]
+    },
 
     // Always leave this as last one,
     // but you can also remove it
