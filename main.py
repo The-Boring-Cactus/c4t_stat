7df648c685f926073666ae41c5f8389daf03b7a9
RicardoRodriguezC4t <69706607+RicardoRodriguezC4t@users.noreply.github.com>
2021-09-29 23:17:05 -0500

    Change to Fask.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import webview
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
 import os
 import sys
 import time
@@ -8,22 +9,44 @@ from src_api.utils.logger import logger
 from src_api.jsapi import Api
 log = logging.getLogger(os.path.basename(__file__))
 
+# configuration
+DEBUG = True
 
-class FStat(object):
-    def __init__(self):
-        self.name = 'F-Stat'
-    
-    def start_Server(self):
-        pass
-    
-    def start_UI(self):
-        api = Api()
-        webview.create_window('F-Stat', 'ui/index.html',  js_api=api)
-        # webview.create_window('F-Stat', 'http://localhost:8080/',  js_api=api)
-        webview.start(debug=True)
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
 
+
+@app.route('/css/<path>', methods=['GET'])
+def ccs(path):
+    return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui', 'css'), path)
+
+
+@app.route('/js/<path>', methods=['GET'])
+def js(path):
+    return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui', 'js'), path)
+
+
+@app.route('/icons/<path>', methods=['GET'])
+def icons(path):
+    return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui', 'icons'), path)
+
+
+@app.route('/lib/<path>', methods=['GET'])
+def lib(path):
+    return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui', 'lib'), path)
+
+
+@app.route('/fonts/<path>', methods=['GET'])
+def fonts(path):
+    return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui', 'fonts'), path)
+
+
+# Index
+@app.route('/', methods=['GET'])
+def index():
+    return send_from_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui'), 'index.html')
 
 
 if __name__ == '__main__':
-    app = FStat()    
-    app.start_UI()
+    app.run(host='0.0.0.0', port=80)
